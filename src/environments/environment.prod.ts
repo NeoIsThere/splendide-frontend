f2a9a5ccc10af7ebd491f49a359811b43d260467
= <neosam777@gmail.com>
2026-04-23 01:30:33 +0200

    bump version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,4 @@ export const environment = {
   googleClientId: '718439408583-ipj7tj3s2genkl1c5uklhmbaq17nqtkk.apps.googleusercontent.com',
 };
 
-export const APP_VERSION = 'v1.0.1';
+export const APP_VERSION = 'v1.0.2';
